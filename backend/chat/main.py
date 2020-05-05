f839e6feca1f7a9bec5cb2c17b4ffe1c222874e9
Андрей Крылосов <ssttoorrmma@gmail.com>
2020-05-05 10:22:43 +0700

    Docker

--- backend/chat/main.py
+++ backend/chat/main.py
@@ -9,4 +9,4 @@ from chat.events.alert import *
 from chat.events.room import *
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=15446)
+    web.run_app(app, host='0.0.0.0', port=443)
